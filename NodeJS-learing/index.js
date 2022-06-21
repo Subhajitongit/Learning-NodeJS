7881c8008925cb57c60b7af9ba7aa966c6fe5562
Subhajit Gorai <90279748+Subhajitongit@users.noreply.github.com>
2022-06-21 17:13:09 +0530

    Add files via upload

---
+++ NodeJS-learing/index.js
@@ -0,0 +1,8 @@
+var superheroes =  require('superheroes');
+var supervillains =  require('supervillains');
+
+var mySuperHero = superheroes.random();
+var mySuperVillain = supervillains.random();
+
+console.log(mySuperHero);
+console.log(mySuperVillain);
